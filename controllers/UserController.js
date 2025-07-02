39d3b7674a0c022f04a0cfe21bbefff47805e6ee
snehithachaluvadi <snehithachaluvadi@gmail.com>
2025-07-03 00:13:42 +0530

    Updated some files

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,32 +1,50 @@
 const User = require('../models/UserModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userController = {
-  signUp: async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      const newUser = new User({ username, password });
-      await newUser.save();
-      res.json('User added!');
-    } catch (error) {
-      res.status(400).json('Error: ' + error.message);
-    }
-  },
+  signUp: async (req, res) => {
+    try {
+      const { username, password } = req.body;
 
-  signIn: async (req, res) => {
-    try {
-      const { username, password } = req.body;
-      const user = await User.findOne({ username, password });
-      
-      if (user) {
-        res.json('Sign-in successful!');
-      } else {
-        res.status(401).json('Invalid credentials');
-      }
-    } catch (error) {
-      res.status(400).json('Error: ' + error.message);
-    }
-  },
-  
+      const existingUser = await User.findOne({ username });
+      if (existingUser) {
+        return res.status(400).json('Username already exists');
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const newUser = new User({ username, password: hashedPassword });
+      await newUser.save();
+      res.json('User added!');
+    } catch (error) {
+      res.status(500).json('Error: ' + error.message);
+    }
+  },
+
+  signIn: async (req, res) => {
+    try {
+      const { username, password } = req.body;
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        return res.status(401).json('Invalid credentials');
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(401).json('Invalid credentials');
+      }
+
+      // Optionally generate token
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET || 'yoursecretkey', {
+        expiresIn: '1h'
+      });
+
+      res.json({ message: 'Sign-in successful!', token });
+    } catch (error) {
+      res.status(500).json('Error: ' + error.message);
+    }
+  }
 };
 
 module.exports = userController;
--- routes/AuctionRoutes.js
+++ routes/AuctionRoutes.js
@@ -2,53 +2,60 @@ const express = require("express");
 const auctionSchema = require("../models/AuctionSchema");
 const auctionRoute = new express.Router();
 
-
-auctionRoute.post("/create-auction",(req,res)=>{
-    auctionSchema.create(req.body,(err,data)=>{
-        if(err)
-            return err;
-        else
+// ✅ Create auction
+auctionRoute.post("/create-auction", (req, res) => {
+    auctionSchema.create(req.body, (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        } else {
             res.json(data);
-    })
+        }
+    });
 });
 
-auctionRoute.get("/",(req,res)=>{
-    auctionSchema.find((err,data)=>{
-        if(err)
-            return err;
-        else
+// ✅ Get all auctions
+auctionRoute.get("/", (req, res) => {
+    auctionSchema.find((err, data) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        } else {
             res.json(data);
-    })
-})
+        }
+    });
+});
+
+// ✅ Get one auction by ID
 auctionRoute.route("/update-auction/:id")
-.get((req,res)=>{
-    auctionSchema.findById(req.params.id,(err,data)=>{
-        if(err)
-            return err;
-        else
-            res.json(data);
-    })
-})
-.put((req,res)=>{
-    auctionSchema.findByIdAndUpdate(req.params.id,{$set:req.body},
-        (err,data)=>{
-            if(err)
-                return err;
-            else
+    .get((req, res) => {
+        auctionSchema.findById(req.params.id, (err, data) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            } else {
                 res.json(data);
-        })
-});
-
-
-auctionRoute.delete("/delete-auction/:id",(req,res)=>{
-    console.log("**** Server delete")
-    auctionSchema.findByIdAndRemove(req.params.id,(err,data)=>{
-        console.log("***** Delete function ", err , data)
-        if(err)
-            return err;
-        else
-            res.json(data);
+            }
+        });
     })
+
+    // ✅ Update auction by ID
+    .put((req, res) => {
+        auctionSchema.findByIdAndUpdate(req.params.id, { $set: req.body }, (err, data) => {
+            if (err) {
+                return res.status(500).json({ error: err.message });
+            } else {
+                res.json(data);
+            }
+        });
+    });
+
+// ✅ Delete auction by ID
+auctionRoute.delete("/delete-auction/:id", (req, res) => {
+    auctionSchema.findByIdAndRemove(req.params.id, (err, data) => {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        } else {
+            res.json(data);
+        }
+    });
 });
 
 module.exports = auctionRoute;
